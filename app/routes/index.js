bfb78ef9c8193cce2e049237888d58f640627b68
Steve-Burton <stevevburton@gmail.com>
2017-01-11 09:24:36 -0800

    add model review.js and update route index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review')
+    });
   },
 
   actions: {
